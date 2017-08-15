8d1b59c5ac71a227974e832cb3bc3a46ec871941
Chris Caldwell <ccaldwell488@gmail.com>
2017-08-15 12:34:24 +0100

    Working solution without MVVM

---
+++ TreeViewTest/HeaderToImageConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace TreeViewTest
+{
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+
+        public object Convert(object value , Type targetType , object parameter , CultureInfo culture)
+        {
+            // check path is valid
+            var path = (string)value;
+            if (path == null)
+                return null;
+
+            // Get the name of the file/folder
+            var name = MainWindow.GetFileFolderName(path);
+
+            // Set default image to file.png
+            var image = "Images/file.png";
+
+            // null or empty path is most likely a drive
+            if (string.IsNullOrEmpty(name))
+                image = "Images/drive.png";
+            // if path is a dir
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+                image = "Images/folder.png";
+
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
+        }
+
+        public object ConvertBack(object value , Type targetType , object parameter , CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
